5552a73cacb8b62a0dd5e2ef8cedf4d9fb321484
晏青 <xibin.wxb@antfin.com>
2022-01-05 21:13:52 +0800

    Code format

--- chap7/printtree.h
+++ chap7/printtree.h
@@ -1,3 +1,9 @@
-/* function prototype from printtree.c */
-void printStmList (FILE *out, T_stmList stmList) ;
+#ifndef PRINTTREE_H_
+#define PRINTTREE_H_
 
+#include "tree.h"
+
+/* function prototype from printtree.c */
+void printStmList(FILE *out, T_stmList stmList);
+
+#endif  // PRINTTREE_H_
